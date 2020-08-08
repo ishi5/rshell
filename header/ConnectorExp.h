1a68edd111de63ec1bd7da640bfa6be8e1b3552c
ishi5 <69340344+ishi5@users.noreply.github.com>
2020-08-08 16:32:00 -0700

    Update ConnectorExp.h

---
+++ header/ConnectorExp.h
@@ -0,0 +1,21 @@
+#ifndef _CONNECTOREXP_H_
+#define _CONNECTOREXP_H_
+
+//#include "../header/Token.h"
+#include "../header/Expression.h"
+#include <vector>
+#include <string>
+#include<iostream>
+
+
+class ConnectorExp: public Expression{
+			
+				public:
+				
+						virtual std::string stringify()=0;
+						virtual bool evaluate()=0;
+				
+
+	};
+
+#endif
